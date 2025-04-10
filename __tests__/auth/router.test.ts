10de76f3ad75786de2570fcccb3027692870363e
Dylan Davies <dyldavies11@gmail.com>
2025-04-10 18:02:53 +0200

    feature: add reauth route

--- __tests__/auth/router.test.ts
+++ __tests__/auth/router.test.ts
@@ -1,4 +1,4 @@
-import { login } from "../../src/auth/router";
+import { login, sessionCheck } from "../../src/auth/router";
 import admin from "firebase-admin";
 
 jest.mock("firebase-admin", () => {
@@ -18,80 +18,150 @@ jest.mock("firebase-admin", () => {
 });
 
 describe('Authentication tests', () => {
-    it("should 400 reject on no authorization header", () => {
-        let req = { headers: {} };
-        let res = { sendStatus: jest.fn() };
+    describe("Session checker method", () => {
+        it("should 200 ok but reject if no __session cookie", () => {
+            let req = { cookies: {} };
+            let res = { status: jest.fn(), send: jest.fn() };
 
-        // @ts-ignore
-        login(req, res);
-        
-        expect(res.sendStatus).toHaveBeenCalledWith(400);
-    });
+            res.status.mockReturnValue(res);
 
-    it("should 400 reject on no bearer authorization header", () => {
-        let req = { headers: {'authorization': "Basic mocktoken"} };
-        let res = { sendStatus: jest.fn() };
+            // @ts-ignore
+            sessionCheck(req, res);
 
-        // @ts-ignore
-        login(req, res);
-        
-        expect(res.sendStatus).toHaveBeenCalledWith(400);
-    });
-
-    it("should 400 reject on malformed bearer authorization header", () => {
-        let req = { headers: {'authorization': "Bearer"} };
-        let res = { sendStatus: jest.fn() };
-
-        // @ts-ignore
-        login(req, res);
-        
-        expect(res.sendStatus).toHaveBeenCalledWith(400);
-    });
-
-
-    it("should 401 reject on failing to verify id token", async () => {
-        let req = { headers: {'authorization': "Bearer mocktoken"} };
-        let res = { status: jest.fn(), send: jest.fn() };
-
-        (res.status as jest.Mocked<any>).mockReturnValue(res);
-        
-        const mockVerifyIdToken = jest.fn().mockRejectedValue(new Error("Invalid token"));
-        (admin.auth as jest.Mock).mockReturnValue({
-            verifyIdToken: mockVerifyIdToken
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith({presence: false});
         });
 
-        // @ts-ignore
-        await login(req, res);
-        
-        expect(res.status).toHaveBeenCalledWith(401);
-        expect(res.send.mock.calls[0][0].message).toBe("Invalid token");
+        it("should 200 ok but reject if error verifying __session", async () => {
+            let req = { cookies: { "__session": "mockSessionCode"} };
+            let res = { status: jest.fn(), send: jest.fn() };
+
+            const verifySessionCookieMock = jest.fn().mockRejectedValue(new Error("auth/could-not-verify"));
+
+            res.status.mockReturnValue(res);
+            (admin.auth as jest.Mock).mockReturnValue({verifySessionCookie: verifySessionCookieMock});
+
+            // @ts-ignore
+            await sessionCheck(req, res);
+
+            expect(verifySessionCookieMock).toHaveBeenCalledWith("mockSessionCode", true);
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith({presence: false});
+        });
+
+        it("should 200 ok but reject if no uid is found after verifying __session", async () => {
+            let req = { cookies: { "__session": "mockSessionCode"} };
+            let res = { status: jest.fn(), send: jest.fn() };
+
+            const verifySessionCookieMock = jest.fn().mockReturnValue({ uid: null });
+
+            res.status.mockReturnValue(res);
+            (admin.auth as jest.Mock).mockReturnValue({verifySessionCookie: verifySessionCookieMock});
+
+            // @ts-ignore
+            await sessionCheck(req, res);
+
+            expect(verifySessionCookieMock).toHaveBeenCalledWith("mockSessionCode", true);
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith({presence: false});
+        });
+
+        it("should 200 ok and accept on valid session", async () => {
+            let req = { cookies: { "__session": "mockSessionCode"} };
+            let res = { status: jest.fn(), send: jest.fn() };
+
+            const verifySessionCookieMock = jest.fn().mockReturnValue({ uid: "mockUId" });
+            const createCustomTokenMock = jest.fn().mockReturnValue("customToken");
+
+            res.status.mockReturnValue(res);
+            (admin.auth as jest.Mock).mockReturnValue({verifySessionCookie: verifySessionCookieMock, createCustomToken: createCustomTokenMock});
+
+            // @ts-ignore
+            await sessionCheck(req, res);
+
+            expect(verifySessionCookieMock).toHaveBeenCalledWith("mockSessionCode", true);
+            expect(createCustomTokenMock).toHaveBeenCalledWith("mockUId");
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith({presence: true, customToken: "customToken"});
+        });
     });
 
-    it("should 200 accept with valid data", async () => {
-        let req = { headers: {'authorization': "Bearer mocktoken"} };
-        let res = { status: jest.fn(), send: jest.fn(), cookie: jest.fn(), sendStatus: jest.fn() };
-
-        const mockVerifyIdToken = jest.fn().mockResolvedValue({ uid: "123" });
-        const mockCreateSessionCookie = jest.fn().mockResolvedValue("mockSessionCookie");
+    describe("Login methods", () => {
+        it("should 400 reject on no authorization header", () => {
+            let req = { headers: {} };
+            let res = { sendStatus: jest.fn() };
     
-        (admin.auth as jest.Mock).mockReturnValue({
-            verifyIdToken: mockVerifyIdToken,
-            createSessionCookie: mockCreateSessionCookie
+            // @ts-ignore
+            login(req, res);
+            
+            expect(res.sendStatus).toHaveBeenCalledWith(400);
         });
-
-        // @ts-ignore
-        await login(req, res);
+    
+        it("should 400 reject on no bearer authorization header", () => {
+            let req = { headers: {'authorization': "Basic mocktoken"} };
+            let res = { sendStatus: jest.fn() };
+    
+            // @ts-ignore
+            login(req, res);
+            
+            expect(res.sendStatus).toHaveBeenCalledWith(400);
+        });
+    
+        it("should 400 reject on malformed bearer authorization header", () => {
+            let req = { headers: {'authorization': "Bearer"} };
+            let res = { sendStatus: jest.fn() };
+    
+            // @ts-ignore
+            login(req, res);
+            
+            expect(res.sendStatus).toHaveBeenCalledWith(400);
+        });
+    
+    
+        it("should 401 reject on failing to verify id token", async () => {
+            let req = { headers: {'authorization': "Bearer mocktoken"} };
+            let res = { status: jest.fn(), send: jest.fn() };
+    
+            (res.status as jest.Mocked<any>).mockReturnValue(res);
+            
+            const mockVerifyIdToken = jest.fn().mockRejectedValue(new Error("Invalid token"));
+            (admin.auth as jest.Mock).mockReturnValue({
+                verifyIdToken: mockVerifyIdToken
+            });
+    
+            // @ts-ignore
+            await login(req, res);
+            
+            expect(res.status).toHaveBeenCalledWith(401);
+            expect(res.send.mock.calls[0][0].message).toBe("Invalid token");
+        });
+    
+        it("should 200 accept with valid data", async () => {
+            let req = { headers: {'authorization': "Bearer mocktoken"} };
+            let res = { status: jest.fn(), send: jest.fn(), cookie: jest.fn(), sendStatus: jest.fn() };
+    
+            const mockVerifyIdToken = jest.fn().mockResolvedValue({ uid: "123" });
+            const mockCreateSessionCookie = jest.fn().mockResolvedValue("mockSessionCookie");
         
-        expect(res.cookie).toHaveBeenCalledWith(
-            "__session",
-            "mockSessionCookie",
-            expect.objectContaining({
-                maxAge: 24 * 60 * 60 * 1000,
-                secure: false,
-                httpOnly: true,
-                sameSite: "strict"
-            })
-        );
-        expect(res.sendStatus).toHaveBeenCalledWith(200);
-    });
+            (admin.auth as jest.Mock).mockReturnValue({
+                verifyIdToken: mockVerifyIdToken,
+                createSessionCookie: mockCreateSessionCookie
+            });
+    
+            // @ts-ignore
+            await login(req, res);
+            
+            expect(res.cookie).toHaveBeenCalledWith(
+                "__session",
+                "mockSessionCookie",
+                expect.objectContaining({
+                    maxAge: 24 * 60 * 60 * 1000,
+                    secure: false,
+                    httpOnly: true,
+                    sameSite: "strict"
+                })
+            );
+            expect(res.sendStatus).toHaveBeenCalledWith(200);
+        });
+    })
 });
--- src/auth/router.ts
+++ src/auth/router.ts
@@ -3,6 +3,27 @@ import { app } from "../firebase";
 
 const router = Router();
 
+const sessionCheck = async (req: Request, res: Response) => {
+    let { __session } = req.cookies;
+
+    if (!__session) {
+        res.status(200).send({presence: false});
+        return;
+    }
+
+    try {
+        const token = await app.auth().verifySessionCookie(__session, true);
+
+        if (token.uid) {
+            const customToken = await app.auth().createCustomToken(token.uid);
+            res.status(200).send({presence: true, customToken});
+        } else res.status(200).send({presence: false});
+    } catch (error) {
+        res.status(200).send({presence: false});
+        return;
+    }
+};
+
 const login = async (req: Request, res: Response) => {
     let authHeader = req.headers['authorization'];
 
@@ -35,6 +56,7 @@ const login = async (req: Request, res: Response) => {
 };
 
 router.post('/login', login);
+router.post('/session', sessionCheck);
 
 export default router;
-export { login };
+export { login, sessionCheck };
