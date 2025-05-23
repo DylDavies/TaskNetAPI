58b01be1f6e0b41fc8e8338d43fd036149fd03a5
Dylan Davies <dyldavies11@gmail.com>
2025-05-23 15:16:14 +0200

    feature: make public safe

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,9 @@
 import admin from 'firebase-admin';
 import serviceAccount from "../firebase-admin.json";
 
+serviceAccount.private_key_id = process.env.PRIVATE_KEY_ID as string;
+serviceAccount.private_key = process.env.PRIVATE_KEY as string;
+
 const app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
 });
