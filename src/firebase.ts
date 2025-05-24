1c4a8eea958715707b1022c22f9ada47e0783bb2
Dylan Davies <dyldavies11@gmail.com>
2025-05-24 16:34:39 +0200

    feature: backend logging

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,6 +4,9 @@ import serviceAccount from "../firebase-admin.json";
 serviceAccount.private_key_id = process.env.PRIVATE_KEY_ID as string;
 serviceAccount.private_key = process.env.PRIVATE_KEY as string;
 
+console.log(process.env.PRIVATE_KEY)
+console.log(process.env.PRIVATE_KEY_ID)
+
 const app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
 });
